52319c0221a9c97d1d778326c90596020ce407d6
amal-san <amaldemaniax@gmail.com>
2020-09-11 22:05:36 +0530

    Updates

---
+++ precache-manifest.11f24dd4cc2f29da27c5f656e1249722.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0b20b06655804844610d32d55c77e6",
+    "url": "/Dphi-nursery/index.html"
+  },
+  {
+    "revision": "9167003f6656cf55db45",
+    "url": "/Dphi-nursery/static/css/2.47b6aeee.chunk.css"
+  },
+  {
+    "revision": "77b3e3511f43ca181880",
+    "url": "/Dphi-nursery/static/css/main.4926e440.chunk.css"
+  },
+  {
+    "revision": "9167003f6656cf55db45",
+    "url": "/Dphi-nursery/static/js/2.f7fe0bf3.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/Dphi-nursery/static/js/2.f7fe0bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b3e3511f43ca181880",
+    "url": "/Dphi-nursery/static/js/main.bf50d4bf.chunk.js"
+  },
+  {
+    "revision": "82939cba17aaf92a0b96",
+    "url": "/Dphi-nursery/static/js/runtime-main.7a19e289.js"
+  }
+]);
